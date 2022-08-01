fccf20cfa1e0163a1d7221bed812a1ac70596d0c
Litway_ <xmw0517@gmail.com>
2022-08-01 23:44:24 +0800

    Jedis_RedisDemo  Jedis选择其他数据库测试 mset|mget

--- Jedis_RedisDemo/src/test/java/com/atguigu/jedis/JedisDemoTest.java
+++ Jedis_RedisDemo/src/test/java/com/atguigu/jedis/JedisDemoTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -14,7 +15,8 @@ public class JedisDemoTest {
 
     @Before
     public void jedis() {
-        this.jedis = new Jedis("192.168.62.131", 6379);
+        this.jedis = new Jedis("localhost", 6379);
+        this.jedis.select(15);
     }
 
 
@@ -24,5 +26,15 @@ public class JedisDemoTest {
         System.out.println(keys);
 
     }
+
+
+    @Test
+    public void stringMset() {
+        jedis.mset("k1", "v1", "k2", "v2");
+        List<String> mget = jedis.mget("k1", "k2");
+        System.out.println(mget);
+
+    }
+
 
 }
