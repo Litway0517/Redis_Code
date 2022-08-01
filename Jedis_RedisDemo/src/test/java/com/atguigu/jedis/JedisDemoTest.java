e3d1e6b3d4d4468cc12f33ffee678e0332449ac6
Litway_ <xmw0517@gmail.com>
2022-08-01 23:53:31 +0800

    Jedis_RedisDemo  操作list

--- Jedis_RedisDemo/src/test/java/com/atguigu/jedis/JedisDemoTest.java
+++ Jedis_RedisDemo/src/test/java/com/atguigu/jedis/JedisDemoTest.java
@@ -16,7 +16,6 @@ public class JedisDemoTest {
     @Before
     public void jedis() {
         this.jedis = new Jedis("localhost", 6379);
-        this.jedis.select(15);
     }
 
 
@@ -27,9 +26,25 @@ public class JedisDemoTest {
 
     }
 
+    // 测试list
+    @Test
+    public void list() {
+        this.jedis.select(14);
+        jedis.rpush("l1", "a", "b", "c");
+        List<String> l1 = jedis.lrange("l1", 0, -1);
+        l1.stream().forEach(System.out::println);
 
+        jedis.rpop("l1");
+        System.out.println(l1);
+
+    }
+
+
+    // 测试字符串
     @Test
     public void stringMset() {
+        this.jedis.select(15);
+
         jedis.mset("k1", "v1", "k2", "v2");
         List<String> mget = jedis.mget("k1", "k2");
         System.out.println(mget);
