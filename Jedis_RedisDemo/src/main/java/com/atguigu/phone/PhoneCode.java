4d31dfab384a9ced7d7c8ee6cd21c4651137a219
Litway_ <xmw0517@gmail.com>
2022-08-03 16:11:02 +0800

    Jedis_RedisDemo  getCode方法

--- Jedis_RedisDemo/src/main/java/com/atguigu/phone/PhoneCode.java
+++ Jedis_RedisDemo/src/main/java/com/atguigu/phone/PhoneCode.java
@@ -1,8 +1,19 @@
 package com.atguigu.phone;
 
+import java.util.Random;
+
 public class PhoneCode {
 
-
+    // 1- 生成6位验证码
+    public static String getCode() {
+        Random random = new Random();
+        StringBuilder code = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            int rand = random.nextInt(10);
+            code.append(rand);
+        }
+        return code.toString();
+    }
 
 
 }
