e149fbbaa59e3b33a6d688ff6b5b70cb437fab80
Litway_ <xmw0517@gmail.com>
2022-08-03 17:14:41 +0800

    Jedis_RedisDemo  测试发送验证码 和 验证

--- Jedis_RedisDemo/src/test/java/com/atguigu/phone/PhoneCodeTest.java
+++ Jedis_RedisDemo/src/test/java/com/atguigu/phone/PhoneCodeTest.java
@@ -2,14 +2,20 @@ package com.atguigu.phone;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class PhoneCodeTest {
 
     @Test
     public void code() {
         String code = PhoneCode.getCode();
         System.out.println(code);
+    }
+
+    @Test
+    public void verifyCode() {
+        String phoneNumber = "13755296636";
+        // PhoneCode.sendCode(phoneNumber);
+        PhoneCode.getRedisCode(phoneNumber, "708563");
+
     }
 
 }
