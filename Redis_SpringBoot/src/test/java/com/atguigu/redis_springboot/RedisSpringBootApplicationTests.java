a2baf0e3b124dda37b98fd884878a07cbae7cf05
Litway_ <xmw0517@gmail.com>
2022-08-04 11:45:50 +0800

    Redis_SpringBoot  RedisConfig配置类 其中内容基本上是固定的 没怎么详细解释 引导类

---
+++ Redis_SpringBoot/src/test/java/com/atguigu/redis_springboot/RedisSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.redis_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
